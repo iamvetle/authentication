b465007a3f23753e6c06d261bf5317bb679a2493
iamvetle <vetle122@gmail.com>
2023-08-05 17:14:00 +0200

    integrated router. also have auth system, but with no use

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,17 +2,17 @@
 
 <h1>This is App.vue</h1>
 
-<router-link to="Home"></router-link>
-<router-link to="Login"></router-link>
+<router-link to="/home">Home</router-link>
+<router-link to="/login">Login</router-link>
 
-<router-view>
+<router-view />
 
-</router-view>
 </template>
 
 <script>
 
 export default {
+
 }
 </script>
 
