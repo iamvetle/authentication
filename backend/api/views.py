344ff185f80592e53dcfbfe3223b5828afe2894e
iamvetle <vetle122@gmail.com>
2023-08-06 19:39:38 +0200

    switched from 'manager app' to 'api app'

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+
 
 # Create your views here.
+
+class CurrentUserView(APIView):
+    permission_classes = [IsAuthenticated] # Checks whether the the token is authenticated/valid
+
+    def get(self, request): # Takes the token and returns the corresponding user information
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
+    
+
+
+# With IsAuthenticated permission, only requests with valid authentication tokens will be allowed to access this endpoint. If the token is missing or invalid, a 401 Unauthorized response will be returned.
--- backend/manager/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from rest_framework.authtoken.views import obtain_auth_token
-from .views import CurrentUserView
-
-urlpatterns = [
-    path("api-token-auth/", obtain_auth_token, name="api_token_auth"), # The built in login view that DRF provides | axios.post to here
-    path("api/users/me/", CurrentUserView.as_view(), name="current-user")
-]
