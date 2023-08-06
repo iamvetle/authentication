bc20118eec6ff13e62e1dc3a49ac328f4147e5d9
iamvetle <vetle122@gmail.com>
2023-08-06 21:50:46 +0200

    add apparant formvalidation with vuelidate, but havent tested yet

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,10 +3,12 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { ValidatePlugin } from '@vuelidate/core'
 
 const app = createApp(App)
 
 app.use(router) // "Adds the router to the application"
 app.use(store)
+app.use(ValidatePlugin)
 
 app.mount('#app')
