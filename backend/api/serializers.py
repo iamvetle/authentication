d4127f0538b7a6484454a7edc32abfea8a7b1742
iamvetle <vetle122@gmail.com>
2023-08-08 00:40:38 +0200

    trying to add a custom usermodel

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,4 +1,5 @@
-# Handle user registration
+# Handles user registration
+# Kind of like a form
 
 from django.contrib.auth.models import User # Built-in django model
 from rest_framework import serializers
@@ -6,5 +7,6 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+
         fields = ['username', 'password', 'first_name', 'last_name', 'email'] #TODO expand this later
         extra_kwargs = {'password': {'write_only': True}} # prevents sending hashed passwords back to web client
