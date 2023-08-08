207d8a4bde726b17b16e1322fbabffeddd14a70f
iamvetle <vetle122@gmail.com>
2023-08-08 12:50:57 +0200

    fixed the register.vue properly, but still cant send

--- frontend/src/components/Register.vue
+++ frontend/src/components/Register.vue
@@ -2,44 +2,49 @@
     <div>
       <form @submit.prevent="submitForm">
         <div>
-          <label for="firstName">First Name:</label>
-          <input class="border" v-model="form.firstName" id="firstName" />
-          <div v-if="!v$.form.firstName.$pending && !v$.form.firstName.required">
+          <label for="first_name">First Name:</label>
+          <input class="border" v-model="first_name" id="first_name"/>
+          <div v-if="v$.first_name.$error">
             First name is required.
           </div>
         </div>
   
         <div>
-          <label for="lastName">Last Name:</label>
-          <input class="border" v-model="form.lastName" id="lastName" />
-          <div v-if="!v$.form.lastName.$pending && !v$.form.lastName.required">
+          <label for="last_name">Last Name:</label>
+          <input class="border" v-model="last_name" id="last_name" />
+          <div v-if="v$.last_name.$error">
             Last name is required.
           </div>
         </div>
   
-        <!-- <div>
-          <label for="username">Username:</label>
-          <input v-model="form.username" id="username" />
-          <div v-if="!v$.form.username.$pending && !v$.form.username.minLength">
-            Username must be at least 3 characters long.
-          </div>
-        </div> -->
-  
         <div>
           <label for="email">Email:</label>
-          <input class="border" v-model="form.email" id="email" type="email" />
-          <div v-if="!v$.form.email.$pending && !v$.form.email.email">
+          <input class="border" v-model="email" id="email" type="email" />
+          <div v-if="v$.email.$error">
             Please enter a valid email address.
           </div>
+        </div>
+
+        <div>
+            <label for="phone">Phone:</label>
+            <input class="border" v-model="phone" id="phone" />
         </div>
   
         <div>
           <label for="password">Password:</label>
-          <input class="border" v-model="form.password" id="password" type="password" />
-          <div v-if="!v$.form.password.$pending && !v$.form.password.minLength">
+          <input class="border" v-model="password" id="password" type="password" />
+          <div v-if="v$.password.$error">
             Password must be at least 6 characters long.
           </div>
         </div>
+
+        <div>
+        <label for="confirmPassword">Confirm Password:</label>
+        <input class="border" v-model="confirmPassword" id="confirmPassword" type="password" />
+        <div class="text-red-500" v-if="v$.confirmPassword.$error">
+          The passwords do not match.
+        </div>
+      </div>
   
         <button class="bg-slate-200" type="submit">Register</button>
       </form>
@@ -47,45 +52,46 @@
   </template>
   
 <script>
-  import { required, minLength, email } from '@vuelidate/validators';
-  import { useVuelidate } from '@vuelidate/core';
-    
-  export default {
+
+import { useVuelidate } from '@vuelidate/core'
+import { required, email, sameAs, minLength } from '@vuelidate/validators' 
+
+export default {
+  setup () {
+    return {
+      v$: useVuelidate()
+    }
+    },    
     data() {
-      return {
-        form: {
-          firstName: '',
-          lastName: '',
-          // username: '',
-          email: '',
-          password: '',
-        },
-      };
-    },
-    validations: {
-      form: {
-        firstName: { required },
-        lastName: { required },
-        // username: { minLength: minLength(3) },
-        email: { email },
-        password: { minLength: minLength(6) },
-      },
-    },
-    setup() {
-      const v$ = useVuelidate();
-      return { v$ };
-    },
-    methods: {
-      submitForm() {
-        this.v$.form.$touch();  // This triggers the validation for all fields
-        if (this.v$.form.$invalid) {
-          alert('Please fix the errors before submitting.');
-        } else {
-          console.log('Form data:', this.form);
+        return {
+            first_name:"",
+            last_name:"",
+            email:"",
+            phone:"",
+            password:"",
+            confirmPassword:"",
         }
+  },
+  validations () {
+    return {
+        first_name:{ required, $autoDirty: true },
+        last_name:{ required, $autoDirty: true },
+        email: { required, email, $autoDirty: true  },
+        phone: { }, 
+        password:{ required, minLength: minLength(6), $autoDirty: true },
+        confirmPassword: {sameAsPassword: sameAs(this.password), $autoDirty: true },
       }
-    },
-  };
+  },
+  methods: {
+    async submitForm() {
+        
+        if (! await this.v$.$validate()) {
+            console.log("Wrong form data:")
+        } else {
+            console.log("Form data:")
+        }
+    }
+  }
+}
+
 </script>
-  
-  
